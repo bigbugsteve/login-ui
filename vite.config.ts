417751bb6c3bdd0a731f2922ab168d2bfbd47d3d
Gábor Nagy <gabor.nagy@ewmgroup.com>
2023-08-25 12:15:16 +0200

    workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	build: {
-		outDir: 'build',
-	},
+export default defineConfig(({ command }) => {
+	const config = {
+		plugins: [react()],
+		base: '/',
+	};
+
+	if (command !== 'serve') {
+		config.base = '/login-ui/';
+	}
+
+	return config;
 });
